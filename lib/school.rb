81f13843f208f54c019e7cf45c0db2ff3ea3ecfa
Sara Kantor <sara.kantor.nv@gmail.com>
2018-10-15 17:51:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  
+  attr_reader :grade
+  
+  ROSTER = {} 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def roster 
+    ROSTER
+  end
+  
+  def add_student(name, grade)
+    if ROSTER.include?(grade)
+      ROSTER[grade] << name
+    else 
+      ROSTER[grade] = [name]
+    end
+  end
+  
+  def grade 
+  
+  end
+  
+  def sort 
+    
+  end
+  
+end
